67ef67a1896df679e0ebdc077d02706ff7b376d9
Obinna Moses <obinnamosco6@gmail.com>
2025-11-18 19:48:17 +0100

    fixed 'path'

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
